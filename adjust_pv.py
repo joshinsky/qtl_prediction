9d02fd14402f13441a23b9683b1998d6a2623258
joshinsky <joshinsky@outlook.de>
2026-02-24 14:20:06 +0100

    add non_sig column to pv_adjust script

--- adjust_pv.py
+++ adjust_pv.py
@@ -21,18 +21,21 @@ start_time = time.time()
 filename = sys.argv[1]          # name of .gz zipped file
 destination = sys.argv[2]	# name of .gz zipped file for results
 pv_cutoff = sys.argv[3]         # user defined pv_cutoff
+pv_cutoff_non_sig = sys.argv[4]	# user defined pv_cutoff for non-significant
 
 # input handling
-if pv_cutoff == '' or filename == '' or destination == '':
-	print("Error: missing input argument pvalue cutoff or file name!")
+if pv_cutoff == '' or non_sig_cutoff == '' or filename == '' or destination == '':
+	print("Error: missing input argument!")
+	print("Usage:\npython3 adjust_pv.py <filename> <destination> <pv_cutoff> <pv_cutoff_non_sig>")
 	sys.exit(1)
 if not filename.endswith('.tsv.gz') or not destination.endswith('.tsv.gz'):
 	print("Error: input file and destination should be in .tsv.gz format")
 	sys.exit(1)
 try: 
 	pv_cutoff = float(pv_cutoff)
+	pv_cutoff_non_sig = float(pv_cutoff_non_sig)
 except ValueError:
-	print("Error: cutoff input should be float, e.g. 0.05")
+	print("Error: pv_cutoff input should be float, e.g. 0.05")
 	sys.exit(1)
 
 
@@ -44,6 +47,7 @@ with gzip.open(filename, 'rt') as eQTL_file:
                 m += 1
 print(f'\nworking with 	m = {m}...')
 print(f'        alpha_adj = {pv_cutoff/m}...')
+print(f'    non_sig_alpha = {pv_cutoff_non_sig}')
 
 
 # prepare pvalue adjustment, define necessary variables
@@ -53,21 +57,26 @@ first_chunk = True
 chunk_num = 0
 sig_tot = 0
 
-
-# store adjusted pvalue and significance bool (1|0)
+# get significant and non-significant entries as bool (1|0)
 for chunk in pd.read_csv(filename, compression='gzip', sep='\t', usecols=['variant', 'gene_id', 'pvalue'], dtype={'variant':'string', 'gene_id':'string', 'pvalue':'float32'}, chunksize=5*10**6):
-	p_adj = np.minimum(chunk['pvalue']*m, 1.0)
+	p_value = chunk['pvalue']
+	p_adj = np.minimum(p_value*m, 1.0)
+
+	# store p_adj, sig and non_sig in .tsv.gz
 	chunk['p_adj'] = p_adj
 	chunk['significant'] = (p_adj <= pv_cutoff).astype(int)
+	chunk['non_significant'] = (p_value >= pv_cutoff_non_sig).astype(int)
 	chunk.to_csv(destination, mode='a', header=first_chunk, compression='gzip', index=False)
+	
 	first_chunk = False
 	chunk_num +=1
 
 	# get stats for continuous output while running
 	sig = chunk['significant'].sum()
-	non_sig = len(chunk)-sig
+	non_sig = chunk['non_significant'].sum()
 	sig_tot += sig
-	print(f"Chunk {chunk_num}/{total_chunks}, sig: {sig}, non_sig: {non_sig}, sig_tot: {sig_tot}")
+	non_sig_tot += non_sig
+	print(f"Chunk {chunk_num}/{total_chunks}, \nsignificant: {sig}, total: {sig_tot} \nnon-significant: {non_sig}, total: {non_sig_tot}")
 
 
 # final message
