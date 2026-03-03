4fbef760c6d6d2c9f29957d5a4fbecec8eb5714e
joshinsky <joshinsky@outlook.de>
2026-03-03 08:53:58 +0100

    make pipeline flow

--- 00pipeline.sh
+++ 00pipeline.sh
@@ -19,13 +19,13 @@ fi
 gunzip -c $raw_file | head -5000001 | gzip > temp/test_subset.tsv.gz
 
 # subset columns
-time bash extract_cols.sh temp/test_subset.tsv.gz temp/test_subset_cols.tsv.gz
+time bash '01getcols.sh' temp/test_subset.tsv.gz temp/test_subset_cols.tsv.gz
 
 # view results
 # gunzip -c temp/test_subset_cols.tsv.gz | head -15
 
 # adjust pvalue and split in sig and non sig
-python3 adjust_pv.py temp/test_subset_cols.tsv.gz temp/test_padj.tsv.gz 0.05 0.9
+python3 '02adjpv.py' temp/test_subset_cols.tsv.gz temp/test_padj.tsv.gz 0.05 0.9
 
 # view results
 # gunzip -c temp/test_padj.tsv.gz | head -25
@@ -33,11 +33,11 @@ python3 adjust_pv.py temp/test_subset_cols.tsv.gz temp/test_padj.tsv.gz 0.05 0.9
 # add sequence and positional data
 
 # split on sig 
-python3 split_sig.py temp/test_padj.tsv.gz temp/test_sig.tsv.gz temp/test_nonsig.tsv.gz 
+python3 '04splitsig.py' temp/test_padj.tsv.gz temp/test_sig.tsv.gz temp/test_nonsig.tsv.gz 
 
 # gunzip -c temp/test_sig.tsv.gz | cut -f5,6 | head -25
 # echo
 # gunzip -c temp/test_nonsig.tsv.gz | cut -f5,6 | head -25
 
 # get most sig variants per gene
-python3 get_most_sig.py temp/test_sig.tsv.gz temp/test_most_sig.tsv.gz
+python3 '05topsig.py' temp/test_sig.tsv.gz temp/test_most_sig.tsv.gz
