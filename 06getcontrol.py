b0314c77dd69b6395da8f637f1a93bc162dc21e6
joshinsky <joshinsky@outlook.de>
2026-03-10 15:05:29 +0000

    modify inner join in get control so it is faster

--- 06getcontrol.py
+++ 06getcontrol.py
@@ -15,7 +15,7 @@ import numpy as np
 def usage():
 	print("\nprogram usage:")
 	print(f"python3 {sys.argv[0]} <positives_filename> <non_sig_filename> <destination> [criteria]")
-	print(f"\ncriteria could be: gene, position, variant")
+	print(f"\ncriteria could be: gene, location, variant")
 	print(f"the order of criteria determines their importance\n")
 	sys.exit(1)
 
@@ -62,17 +62,15 @@ if len(criteria_args) == 0:
 ################################
 
 # list available criteria
-criterion_to_col = {
-    "gene":     "gene_id",
-    "position": "rel_pos",
-    "variant":     "type"
-}
+criterion_to_col = {"gene":		"gene_id",
+					"location":	"variant_location",
+					"variant":	"type"}
 
 # get criteria to use in a ranked list
 criteria_used = []
 for c in criteria_args:
-    if c in criterion_to_col:
-        criteria_used.append(criterion_to_col[c])
+	if c in criterion_to_col:
+		criteria_used.append(criterion_to_col[c])
 
 # handle unusable/invalid criteria
 if len(criteria_used) == 0:
@@ -87,55 +85,6 @@ else:
 	print(f"Using: {criteria_used}\n")
 
 
-# #########################################
-# ##          03 - Get negatives         ##
-# ## (find most similar non sig entries) ##
-# #########################################
-
-# # load data
-# print(f"loading data...\n")
-# pos_df = pd.read_csv(positives_filename, compression='gzip', sep='\t')
-# nsig_df = pd.read_csv(non_sig_filename, compression='gzip', sep='\t')
-
-# # initialise loop
-# control_rows = []  
-# criteria_cols = [criterion_to_col[crit] for crit in criteria_used]
-
-# # for each significant entry find the best (most similar) negative control
-# print(f"finding negative controls for {len(pos_df.index)} significant entries...\n")
-# for pos_idx in range(len(pos_df)):
-#     pos_row = pos_df.iloc[pos_idx]
-    
-#     best_match_vector = [-1] * len(criteria_used)
-#     best_candidates = []
-    
-#     for neg_idx in range(len(nsig_df)):
-#         neg_row = nsig_df.iloc[neg_idx]
-
-#         # get match between columns as a binary vector
-#         match_vector = [1 if pos_row[col] == neg_row[col] else 0 for col in criteria_cols]
-        
-#         # test if current match is better than current best match
-#         if match_vector > best_match_vector:
-#             best_match_vector = match_vector[:]
-#             best_candidates = [neg_row]
-#         elif match_vector == best_match_vector:
-#             best_candidates.append(neg_row)
-    
-#     # Pick first best candidate
-#     control = best_candidates[0]
-#     control_rows.append(control)
-#     print(f"found match for {pos_row['gene_id']} --> matching vector = {best_match_vector}:")
-
-# print(f"Storing results")
-# control_df = pd.DataFrame(control_rows)
-# control_df.to_csv(destination_filename, sep='\t', index=False, compression='gzip')
-
-# print(f"Finished! results can be found at {destination_filename}")
-# print(f"Confirm using:\ngunzip -c {destination_filename} | head")
-
-
-
 #########################################
 ##          03 - Get negatives         ##
 ## (find most similar non sig entries) ##
@@ -147,43 +96,19 @@ print(f"loading data...\n")
 pos_df = pd.read_csv(positives_filename, compression='gzip', sep='\t')
 nsig_df = pd.read_csv(non_sig_filename, compression='gzip', sep='\t')
 
-negatives_df = pd.merge(pos_df, nsig_df, how='inner', on=criteria_used)
+neg_df = pd.merge(pos_df, nsig_df, how='inner', on=criteria_used)
 
+if len(neg_df.index) < len(pos_df.index):
+	print("could not find negative control for all positive controls!")
+	print(f"found {len(neg_df.index)} negatives for {len(pos_df.index)} positives.")
 
+# keep only the first gene in case of multiple perfect matches
+neg_df = neg_df.drop_duplicates(subset=['gene_id'])
 
+print(f"Storing results")
+# control_df = pd.DataFrame(control_rows)
+neg_df.to_csv(destination_filename, sep='\t', index=False, compression='gzip')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f"Finished! results can be found at {destination_filename}")
+print(f"Confirm using:\ngunzip -c {destination_filename} | head")
 
