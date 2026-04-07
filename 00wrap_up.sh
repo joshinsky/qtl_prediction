de33fb67be9b8be17d375cc3bc035db3ab28022a
joshinsky <joshinsky@outlook.de>
2026-04-07 09:27:55 +0200

    force node08

--- 00wrap_up.sh
+++ 00wrap_up.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=wrapup
+#SBATCH --nodelist=node08
 #SBATCH --output=/home/projects2/kvs_students/2026/jl_qtl_prediction/repo/qtl_prediction/logs/wrapup-%j.out
 #SBATCH --error=/home/projects2/kvs_students/2026/jl_qtl_prediction/repo/qtl_prediction/logs/wrapup-%j.err
 #SBATCH --time=200:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
+#SBATCH --mem=15G
 
 set -euo pipefail
 
