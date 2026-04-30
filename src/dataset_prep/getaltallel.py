80f11cc026a608f11e368c7f0909ad6109bb8b33
joshinsky <joshinsky@outlook.de>
2026-04-30 13:46:45 +0200

    modify to enable multiple seq windows

--- src/dataset_prep/getaltallel.py
+++ src/dataset_prep/getaltallel.py
@@ -4,17 +4,14 @@ import pandas as pd
 import numpy as np
 import sys
 
-# load dataset
-df = pd.read_csv("results/output/dataset_prep/deduplicated_dataset.tsv.gz", sep='\t', compression='gzip')
-
-def inject_variant(row):
+def inject_variant(row, window_len=100):
     # variant ID is formatted as 'chr_pos_ref_alt'
     var_parts = row['variant'].split('_')
     ref_allele = var_parts[2].upper()
     alt_allele = var_parts[3].upper()
     
     # Get the wild-type sequence
-    ref_seq = row['variant_window'].upper()
+    ref_seq = row[f'variant_window_{window_len}_ref'].upper()
     
     # ensure the reference allele actually matches the sequence at index 100
     expected_ref_in_seq = ref_seq[100:100+len(ref_allele)]
@@ -29,13 +26,15 @@ def inject_variant(row):
     
     return alt_seq
 
+# load dataset
+df = pd.read_csv("results/output/dataset_prep/deduplicated_dataset.tsv.gz", sep='\t', compression='gzip')
+
 print("Injecting ALT alleles to create variant sequences...")
-df['variant_window_alt'] = df.apply(inject_variant, axis=1)
+df[f'variant_window_20_alt'] = df.apply(inject_variant(window_len=20), axis=1)
+df[f'variant_window_100_alt'] = df.apply(inject_variant(window_len=100), axis=1)
+df[f'variant_window_1000_alt'] = df.apply(inject_variant(wondow_len=1000), axis=1)
 
-# Drop any rows where the sanity check failed (if necessary)
-df = df.dropna(subset=['variant_window_alt'])
-
-print("Saving updated dataset...")
 # Save the new dataset
+print("Saving updated dataset...")
 df.to_csv("results/output/dataset_prep/final_full_dataset.tsv.gz", sep='\t', index=False, compression='gzip')
 print("Done!")
