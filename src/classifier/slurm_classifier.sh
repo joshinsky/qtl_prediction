249006274b713e25be39c07e6f1ea2d3afd18a72
joshinsky <joshinsky@outlook.de>
2026-05-06 13:26:01 +0200

    update classifier sbatch to run 4 jobs at once

--- src/classifier/slurm_classifier.sh
+++ src/classifier/slurm_classifier.sh
@@ -12,7 +12,7 @@
 #SBATCH --time=200:00:00
 
 # Job array 
-#SBATCH --array=0-31%8
+#SBATCH --array=0-31%4
 
 # Output files:
 #SBATCH --output=/home/projects2/kvs_students/2026/jl_qtl_prediction/repo/qtl_prediction/logs/classifier/class-%A_%a.out
